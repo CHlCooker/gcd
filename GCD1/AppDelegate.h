106c9952c9802fc61dce6e147ec42ef8cea033a5
CHlCooker <changhongliang@changhongliangdemacbook-pro.local>
2016-08-04 23:34:50 +0800

    Initial Commit

---
+++ GCD1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD1
+//
+//  Created by 常红亮 on 16/8/4.
+//  Copyright © 2016年 RIch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD1
+//
+//  Created by 常红亮 on 16/8/4.
+//  Copyright © 2016年 RIch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
