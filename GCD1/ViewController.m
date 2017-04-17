bac6ad7b6306a4fe95d13fcd1346ed4a6e201510
CHlCooker <changhongliang@changhongliangdeMacBook-Pro.local>
2017-04-17 20:19:58 +0800

    初始化代码

--- GCD1/ViewController.m
+++ GCD1/ViewController.m
@@ -17,11 +17,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
 }
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    [self syncSerial];
 }
+- (void)syncSerial
+{
+    NSLog(@"我被执行了");
+    // 1.创建一个串行队列
+    // #define DISPATCH_QUEUE_SERIAL NULL
+    // 所以可以直接传NULL
+    dispatch_queue_t queue = dispatch_queue_create("com.520it",DISPATCH_QUEUE_SERIAL);
+//    dispatch_queue_t queue = dispatch_get_main_queue();
+    
+    // 2.将任务添加到队列中
+    dispatch_sync(queue, ^{
+        NSLog(@"任务1  == %@", [NSThread currentThread]);
+//        for (NSInteger i = 0; i<100; i++) {
+//            NSLog(@"这是第%ld次打印",(long)i);
+//            NSLog(@"%@",[NSThread currentThread]);
+//        }
+        NSLog(@"我是猪队列中的");
 
+    });
+//    dispatch_sync(queue, ^{
+//        NSLog(@"任务2  == %@", [NSThread currentThread]);
+//        for (NSInteger i = 0; i<100; i++) {
+//            NSLog(@"这是任务二%ld次打印",(long)i);
+//            NSLog(@"%@",[NSThread currentThread]);
+//        }
+//        
+//    });
+//    dispatch_sync(queue, ^{
+//        NSLog(@"任务3  == %@", [NSThread currentThread]);
+//    });
+//    for (NSInteger i = 0; i<100; i++) {
+//        NSLog(@"这是任务三%ld次打印",(long)i);
+//        NSLog(@"%@",[NSThread currentThread]);
+//    }
+    
+    NSLog(@"---------");
+}
 @end
